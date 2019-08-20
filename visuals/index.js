cdfafdd5d3677de09926bd84bc04f5e7ab69b3fd
Thomas Lisankie <thomas@tomlisankie.com>
2019-08-20 19:37:29 -0400

    Added function to regenerate interface to show highlight in different position

--- visuals/index.js
+++ visuals/index.js
@@ -30,16 +30,44 @@ function removeStringsFromText (stringsToRemove, text) {
     }
 }
 
-function generateInterface () {
+function generateInterface (pos) {
     const sourceDiv = document.getElementById ("source");
     const sourceString = "var hi = 'hello there';";
-    const highlightedChar = <span className="highlight" children="h" />
+    var beginningOfSource = sourceString.substring (0, pos - 1);
+    var highlightedChar = sourceString.substring (pos - 1, pos);
+    var restOfSource =  sourceString.substring (pos);
+    const highlightedCharSpan = <span className="highlight" children={highlightedChar} />;
+    console.log (sourceString.substring (3));
     const sourceCodeProps = {
 	className : "sourceCode",
-	children : [sourceString, highlightedChar]
+	children : [beginningOfSource, highlightedCharSpan, restOfSource]
     }
     const sourceCodeDiv = <div {...sourceCodeProps} />
     ReactDOM.render (sourceCodeDiv, sourceDiv);
 }
 
-generateInterface ();
+const sourceString = "var hi = 'hello there';";
+
+function regenerateInterface (pos, waitTime) {
+    setTimeout (() => {
+	const sourceDiv = document.getElementById ("source");
+	var beginningOfSource = sourceString.substring (0, pos - 1);
+	var highlightedChar = sourceString.substring (pos - 1, pos);
+	var restOfSource =  sourceString.substring (pos);
+	const highlightedCharSpan = <span className="highlight" children={highlightedChar} />;
+	console.log (sourceString.substring (3));
+	const sourceCodeProps = {
+	    className : "sourceCode",
+	    children : [beginningOfSource, highlightedCharSpan, restOfSource]
+	}
+	const sourceCodeDiv = <div {...sourceCodeProps} />
+	ReactDOM.render (sourceCodeDiv, sourceDiv);
+    }, waitTime * 1000);
+}
+
+generateInterface (0);
+regenerateInterface (i, 0.25);
+// for (var i = 0; i < sourceString.length; i++) {
+//     setTimeout (() => regenerateInterface (i, 0.25), 500);
+// }
+
