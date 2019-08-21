742c17fd2fb0a470ad67d03be0c0c8eb65d3c485
Thomas <thomas.lisankie@gmail.com>
2019-08-21 14:08:17 -0400

    Removed highlightEachChar, highlightCharAt, and removeStringsFromText functions since I'm now using React for all highlighting functionality.

--- visuals/index.js
+++ visuals/index.js
@@ -1,35 +1,3 @@
-function highlightEachChar (divName) {
-    const source = document.getElementById (divName).innerHTML.trim();
-    for (var i = 0; i < source.length; i++) {
-	console.log ("i: ", i);
-	setTimeout(function(){
-    	    highlightCharAt (i, divName);
-	}, 500);
-    }
-}
-
-function highlightCharAt (position, divName) {
-    var inputTextDiv = document.getElementById (divName);
-    var innerHTML = inputTextDiv.innerHTML.trim();
-    const source = removeStringsFromText (["<span class=\"highlight\">", "</span>", "amp;", "amp"], innerHTML);
-    if (position >= 0) {
-        innerHTML = source.substring (0, position) + "<span class='highlight'>" + source.substring (position, position + 1) + "</span>" + source.substring (position + 1);
-        inputTextDiv.innerHTML = innerHTML;
-    }
-    
-}
-
-function removeStringsFromText (stringsToRemove, text) {
-    if (stringsToRemove.length == 0) {
-	    return text;
-    } else {
-        const [head, ...rest] = stringsToRemove;
-        const newText = text.replace (new RegExp (head, ['g']), "");
-        removeStringsFromText (rest, newText);
-        return newText;
-    }
-}
-
 const sourceString = "var hi = 'hello there';";
 
 function generateInterface (pos) {
