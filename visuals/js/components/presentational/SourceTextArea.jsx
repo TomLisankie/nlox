d9bbfd74ecb5c19e236c21a8446c861de6bec89e
Thomas Lisankie <thomas@tomlisankie.com>
2019-08-22 16:09:29 -0400

    Now text area has default text

--- visuals/js/components/presentational/SourceTextArea.jsx
+++ visuals/js/components/presentational/SourceTextArea.jsx
@@ -3,8 +3,7 @@ import PropTypes from "prop-types";
 
 const SourceTextArea = function ({defaultText, rows, columns}) {
     return (
-	<textarea autoFocus className={"source-text-area"} rows={rows} cols={columns} placeholder={defaultText}>
-	  {"var hi = 'hello there';"}
+	<textarea autoFocus className={"source-text-area"} rows={rows} cols={columns} placeholder={defaultText} defaultValue={defaultText}>
 	  </textarea>
     );
 }
