328da31026332c12b920a03c098a568cc5dbf622
Thomas Lisankie <thomas@tomlisankie.com>
2019-08-26 14:52:40 -0400

    Now properly switches between source text area and the div for scanning the source when button clicked

--- visuals/js/components/container/SourceContainer.jsx
+++ visuals/js/components/container/SourceContainer.jsx
@@ -3,63 +3,60 @@ import ReactDOM from "react-dom";
 import SourceTextArea from "../presentational/SourceTextArea.jsx";
 import Button from "../presentational/Button.jsx";
 import SourceDiv from "../presentational/SourceDiv.jsx";
+import HighlightedChar from "../presentational/HighlightedChar.jsx";
 
 import Scanner from "../../../../src/Scanner.js";
 
 class SourceContainer extends Component {
-    constructor () {
-	super ();
+    constructor (props) {
+	super (props);
 
 	this.state = {
-	    scanning : false,
-	    editing : true
+	    scanning : false
 	};
+
+	this.scanButtonClicked = () => {
+
+	    if (this.state.scanning) {
+		this.setState ({scanning : false});
+	    } else {
+		this.setState({scanning : true});
+		const scanner = new Scanner ();
+		scanner.scanTokens (document.querySelector (".source-text-area").value);
+		// Source has now been scanned and tokens lexed out
+		// Job of interface is to visually replicate the state at every point where there was a change to the state.
+		console.log (scanner.shelf);
+		const numOfStates = scanner.shelf.states.getLength ();
+		for (var i = 0; i < numOfStates; i++) {
+		    const currentState = scanner.shelf.getNextState ();
+		    //construct render with corresponding characters highlighted
+		}
+	    }
+
+	}
     }
 
     render () {
 	return (
 	    <div className="source-container">
-	      <SourceTextArea
-		rows={10}
-		columns={60}
-		defaultText="var hi = 'hello there';" />
-	      <SourceDiv
-		style="display:none;"
-		text="" />
+	      {
+		  this.state.scanning ?
+		      <SourceDiv
+			    style={{display : "block"}}
+			    text={document.querySelector(".source-text-area").value} indexOfCharToHighlight={3} />
+			  :
+			  <SourceTextArea
+				rows={10}
+				columns={60}
+				defaultText="var hi = 'hello there';" />
+	      }
 	      <Button
-		onClickFunc={beginScanning}
+		onClickFunc={this.scanButtonClicked}
 		text={"Scan source"} />
 	    </div>
 	);
     }
     
-}
-
-function beginScanning () {
-    function hideTextArea () {
-	document.querySelector (".source-text-area").setAttribute ("style", "display:none;");
-    }
-
-    function showSourceDiv () {
-	const sourceDiv = document.querySelector (".source-div");
-	sourceDiv.setAttribute ("style", "display:block;");
-	sourceDiv.innerHTML = document.querySelector (".source-text-area").value;
-    }
-
-    hideTextArea ();
-    showSourceDiv ();
-
-    const scanner = new Scanner ();
-    scanner.scanTokens (document.querySelector (".source-text-area").value);
-    // Source has now been scanned and tokens lexed out
-    // Job of interface is to visually replicate the state at every point where there was a change to the state.
-    console.log (scanner.shelf);
-    const numOfStates = scanner.shelf.states.getLength ();
-    for (var i = 0; i < numOfStates; i++) {
-	const currentState = scanner.shelf.getNextState ();
-	//construct render with corresponding characters highlighted
-    }
-    // start highlighting
 }
 
 
--- visuals/js/components/presentational/SourceDiv.jsx
+++ visuals/js/components/presentational/SourceDiv.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SourceDiv = function ({text}) {
+import HighlightedChar from "./HighlightedChar.jsx";
+
+const SourceDiv = function ({style, text, indexOfCharToHighlight}) {
+
+    var beginningOfSource = text.substring (0, indexOfCharToHighlight - 1);
+    var charToBeHighlighted = text.substring (indexOfCharToHighlight - 1, indexOfCharToHighlight);
+    var restOfSource =  text.substring (indexOfCharToHighlight);
+
+    console.log (charToBeHighlighted);
+    
     return (
-	<div className={"source-div"}>
-	  {text}
+	<div style={style} className={"source-div"}>
+	  {beginningOfSource}
+	  <HighlightedChar className="highlight" character={charToBeHighlighted} />
+	  {restOfSource}
 	</div>
     );
 }
 
 SourceDiv.propTypes = {
-    text : PropTypes.string.isRequired
+    text : PropTypes.string.isRequired,
+    indexOfCharToHighlight : PropTypes.number.isRequired
 };
 
 export default SourceDiv;
