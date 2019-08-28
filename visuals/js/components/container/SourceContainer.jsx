d351e398cc1c91db2dfbcf9df86cee0720f0b496
Thomas Lisankie <thomas@tomlisankie.com>
2019-08-28 17:18:50 -0400

    Added a separate container for lexed tokens to go

--- visuals/js/components/container/SourceContainer.jsx
+++ visuals/js/components/container/SourceContainer.jsx
@@ -4,6 +4,7 @@ import SourceTextArea from "../presentational/SourceTextArea.jsx";
 import Button from "../presentational/Button.jsx";
 import SourceDiv from "./SourceDiv.jsx";
 import HighlightedChar from "../presentational/HighlightedChar.jsx";
+import TokenDiv from "./TokenDiv.jsx";
 
 import Scanner from "../../../../src/Scanner.js";
 
@@ -40,27 +41,41 @@ class SourceContainer extends Component {
 
     render () {
 	return (
-	    <div className="source-container">
-	      {
-		  this.state.scanning ?
-		      <div className="scanning">
-		      <SourceDiv
-			    text={document.querySelector(".source-text-area").value} initiallyHighlightedIndex={1}/>
-			  <Button
-				    onClickFunc={this.editAndRestartButtonClicked}
-				    text={"Edit source and restart"} />
-			  </div>
-			  :
-			  <div className="editing">
-			  <SourceTextArea
-				rows={10}
-				columns={60}
-				defaultText="var hi = 'hello there';" />
-			      <Button
-				    onClickFunc={this.scanButtonClicked}
-				    text={"Scan source"} />
-			      </div>
-	      }
+	    <div id="page">
+	      <div id="source-container" className="col-md-6">
+		{
+		    this.state.scanning ?
+			<div className="scanning">
+			      <SourceDiv
+				    text={document.querySelector(".source-text-area").value} initiallyHighlightedIndex={1}/>
+				  <Button
+					onClickFunc={this.editAndRestartButtonClicked}
+					text={"Edit source and restart"} />
+			    </div>
+			    :
+			    <div className="editing">
+				  <SourceTextArea
+					rows={10}
+					columns={60}
+					defaultText="var hi = 'hello there';" />
+				      <Button
+					    onClickFunc={this.scanButtonClicked}
+					    text={"Scan source"} />
+				</div>
+			    }
+	      </div>
+	      <div id="token-container" className="col-md-6">
+		{
+		    this.state.scanning ?
+			<div className="scanned-tokens">
+			      <TokenDiv />
+			    </div>
+			    :
+			    <div className="no-scanned">
+				  To start scanning, click the "Scan source" button on the left.
+				</div>
+		}
+	      </div>
 	    </div>
 	);
     }
---
+++ visuals/js/components/container/TokenDiv.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class TokenDiv extends React.Component {
+    
+    constructor (props) {
+	super (props);
+    }
+
+    render () {
+	return (
+	    <div className="token-div">
+	      Tokens will appear here.
+	    </div>
+	);
+    }
+    
+}
+
+export default TokenDiv;
